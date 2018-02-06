6544ef4b199609b6875a18c6a6a78531711206dd
Anton Ivanov <a.ivanov@seraphim.online>
2018-02-06 21:56:49 +0300

    Fixes and delete

--- tree/avl_tree.h
+++ tree/avl_tree.h
@@ -1,5 +1,7 @@
 #include <algorithm>
 #include <functional>
+#include <memory>
+#include <utility>
 
 #ifndef TREE_AVL_TREE_H
 #define TREE_AVL_TREE_H
@@ -14,25 +16,29 @@ namespace {
       unsigned char height;
       K key;
       V value;
-      avl_node* left;
-      avl_node* right;
+      std::unique_ptr<avl_node<K, V>> left;
+      std::unique_ptr<avl_node<K, V>> right;
     };
 
-    template<typename K, typename V> char height(avl_node<K, V>* node) {
+    template<typename K, typename V> char height(const std::unique_ptr<avl_node<K, V>>& node) {
       return node ? node->height : 0;
     }
 
-    template<typename K, typename V> char balance_factor(avl_node<K, V>* node) {
+    template<typename K, typename V> char balance_factor(const std::unique_ptr<avl_node<K, V>>& node) {
       return height(node->right) - height(node->left);
     }
 
-    template<typename K, typename V> void recalculate_height(avl_node<K, V>* node) {
+    template<typename K, typename V> void recalculate_height(std::unique_ptr<avl_node<K, V>>& node) {
       char l_height = height(node->left);
       char r_height = height(node->right);
       node->height = std::max(l_height, r_height) + 1;
     }
 
-    template<typename K, typename V> avl_node<K, V>* small_right_rotattion(avl_node<K, V>* node) {
+    template<typename K, typename V> std::unique_ptr<avl_node<K, V>>& find_min_node(std::unique_ptr<avl_node<K, V>>& node) {
+      return node->left ? find_min_node(node->left) : node;
+    }
+
+    template<typename K, typename V> std::unique_ptr<avl_node<K, V>> small_right_rotattion(std::unique_ptr<avl_node<K, V>>& node) {
       /*
       Used when balance_factor == 2 and height(q->right) <= height(q->left)
                         p
@@ -57,31 +63,31 @@ namespace {
                   \   /   \
                   d  b     c
       */
-      avl_node<K, V>* new_root = node->left;
-      node->left = new_root->right;
-      new_root->right = node;
-      recalculate_height(node);
+      std::unique_ptr<avl_node<K, V>> new_root = std::move(node->left);
+      node->left = std::move(new_root->right);
+      new_root->right = std::move(node);
+      recalculate_height(new_root->right);
       recalculate_height(new_root);
       return new_root;
     }
 
-    template<typename K, typename V> avl_node<K, V>* small_left_rotation(avl_node<K, V>* node) {
+    template<typename K, typename V> std::unique_ptr<avl_node<K, V>> small_left_rotation(std::unique_ptr<avl_node<K, V>>& node) {
       // Is a mirror of small_right_rotattion
-      avl_node<K, V>* new_root = node->right;
-      node->right = new_root->left;
-      new_root->left = node;
-      recalculate_height(node);
+      std::unique_ptr<avl_node<K, V>> new_root = std::move(node->right);
+      node->right = std::move(new_root->left);
+      new_root->left = std::move(node);
+      recalculate_height(new_root->left);
       recalculate_height(new_root);
       return new_root;
     }
 
-    template<typename K, typename V> avl_node<K, V>* balance(avl_node<K, V>* node) {
+    template<typename K, typename V> std::unique_ptr<avl_node<K, V>> balance(std::unique_ptr<avl_node<K, V>>& node) {
       recalculate_height(node);
       // Left rotation needed
       if (balance_factor(node) == 2) {
         // R < C. Need big rotation
         if (balance_factor(node->right) < 0)
-          node->right = small_right_rotattion(node->right);
+          node->right = std::move(small_right_rotattion(node->right));
         return small_left_rotation(node);
       }
 
@@ -89,30 +95,36 @@ namespace {
       if (balance_factor(node) == -2) {
         // C > L. Big rotation needed
         if (balance_factor(node->left) > 0)
-          node->left = small_left_rotation(node->left);
+          node->left = std::move(small_left_rotation(node->left));
         return small_right_rotattion(node);
       }
 
-      return node;
+      return std::move(node);
     }
 }
 
 namespace avl_utils {
   template<typename K, typename V, typename Compare = std::less<K>, typename Equality = std::equal_to<K>> class avl_tree {
   public:
+
+    avl_tree() : root(nullptr) {}
+
     void insert(const K& key, const V& value) {
       root = insert(root, key, value);
     }
 
-    void remove(const K& key);
-    V lookup(const K& key) const throw (std::out_of_range) {
+    void remove(const K& key) {
+      root = remove(root, key);
+    }
+
+    V lookup(const K& key) const {
       return lookup(root, key);
     }
 
   private:
 
-    V lookup(avl_node<K, V>* node, const K& key) const {
-      if (node == nullptr)
+    V lookup(const std::unique_ptr<avl_node<K, V>>& node, const K& key) const {
+      if (!node)
         throw std::out_of_range("Can't find such an element");
       if (equal_to(node->key, key))
         return node->value;
@@ -123,12 +135,47 @@ namespace avl_utils {
       }
     }
 
-    avl_node<K, V>* insert(avl_node<K, V>* node, const K& key, const V& value) const throw (std::out_of_range) {
+    std::unique_ptr<avl_node<K, V>> remove_min_node(std::unique_ptr<avl_node<K, V>> &node) {
+      if (!node->left)
+        return remove_node(node);
+      node->left = remove_min_node(node->left);
+      return std::move(balance(node));
+    }
+
+    std::unique_ptr<avl_node<K, V>> remove_node(std::unique_ptr<avl_node<K, V>> &node) {
+      // 1st case: don't have right neithbourgh
+      if (!node->right)
+        return std::move(node->left);
+      // 2nd case: have right neighbourgh
+      auto& min_node = find_min_node(node->right);
+      node->key = min_node->key;
+      node->value = min_node->value;
+      node->right = std::move(remove_min_node(node->right));
+      return std::move(balance(node));
+    }
+
+    std::unique_ptr<avl_node<K, V>> remove(std::unique_ptr<avl_node<K, V>>& node, const K& key) {
+      if (!node)
+        throw std::out_of_range("Can't find node to delete");
+
+      // Found node to delete
+      if (equal_to(key, node->key)) {
+        return std::move(remove_node(node));
+      } else if (compare(key, node->key)) {
+        node->left = std::move(remove(node->left, key));
+      } else {
+        node->right = std::move(remove(node->right, key));
+      }
+
+      return std::move(balance(node));
+    }
+
+    std::unique_ptr<avl_node<K, V>> insert(std::unique_ptr<avl_node<K, V>>& node, const K& key, const V& value) const {
       if (node == nullptr)
-        return new avl_node<K, V>(key, value);
+        return std::make_unique<avl_node<K, V>>(key, value);
       if (equal_to(node->key, key)) {
         node->value = value;
-        return node;
+        return std::move(node);
       }
       if (compare(key, node->key)) {
         node->left = insert(node->left, key, value);
@@ -139,7 +186,7 @@ namespace avl_utils {
       return balance(node);
     }
 
-    avl_node<K, V>* root = nullptr;
+    std::unique_ptr<avl_node<K, V>> root;
     Compare compare;
     Equality equal_to;
   };
--- tree/test.cc
+++ tree/test.cc
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <map>
 #include <chrono>
-#include <ctime>
 #include <vector>
+#include <unordered_set>
 
 #include "avl_tree.h"
+
+namespace {
+  constexpr int loops = 20000000;
+}
 
 int main() {
   std::srand(std::time(nullptr));
@@ -12,11 +16,16 @@ int main() {
   avl_utils::avl_tree<int, float> avl;
   std::map<int, float> rb;
   std::vector<int> vals_to_insert;
-  for (int i = 0; i < 20000000; i++) {
-    vals_to_insert.push_back(std::rand());
+  std::unordered_set<int> dups;
+  for (int i = 0; i < loops; i++) {
+    int r = std::rand();
+    while (dups.find(r) != dups.end())
+      r = std::rand();
+    vals_to_insert.push_back(r);
+    dups.insert(r);
   }
   auto start = std::chrono::duration_cast< std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
-  for (size_t i = 0; i < 20000000; i++) {
+  for (size_t i = 0; i < loops; i++) {
     avl.insert(vals_to_insert[i], i);
   }
   auto end = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
@@ -24,7 +33,7 @@ int main() {
   std::cout << "AVL insert: " << (end - start).count() << std::endl;
 
   start = std::chrono::duration_cast< std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
-  for (size_t i = 0; i < 20000000; i++) {
+  for (size_t i = 0; i < loops; i++) {
     rb.insert(std::make_pair(vals_to_insert[i], i));
   }
   end = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
@@ -32,7 +41,7 @@ int main() {
 
 
   start = std::chrono::duration_cast< std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
-  for (size_t i = 0; i < 20000000; i++) {
+  for (size_t i = 0; i < loops; i++) {
     avl.lookup(vals_to_insert[i]);
   }
   end = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
@@ -40,12 +49,29 @@ int main() {
   std::cout << "AVL lookup: " << (end - start).count() << std::endl;
 
   start = std::chrono::duration_cast< std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
-  for (size_t i = 0; i < 20000000; i++) {
+  for (size_t i = 0; i < loops; i++) {
     rb.at(vals_to_insert[i]);
   }
   end = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
 
   std::cout << "RB lookup: " << (end - start).count() << std::endl;
+
+  start = std::chrono::duration_cast< std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+  for (size_t i = 0; i < loops; i++) {
+    avl.remove(vals_to_insert[i]);
+  }
+  end = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+
+  std::cout << "AVL remove: " << (end - start).count() << std::endl;
+
+
+  start = std::chrono::duration_cast< std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+  for (size_t i = 0; i < loops; i++) {
+    rb.erase(vals_to_insert[i]);
+  }
+  end = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+
+  std::cout << "RB remove: " << (end - start).count() << std::endl;
 
   return 0;
 }
